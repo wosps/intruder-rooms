e9b3ab68a1a270ef52a3578070691aa17cce8c8a
Jord <7325535+wosps@users.noreply.github.com>
2023-03-22 21:26:16 +0000

    Add more stats to profile

--- pages/profile/[steamid].tsx
+++ pages/profile/[steamid].tsx
@@ -52,6 +52,22 @@ type StatsProps = {
     totalXp: number,
 }
 
+type VoteProps = {
+    [0]: {
+    id: number,
+    agentId: number,
+    attributeId: number,
+    positive: number,
+    negative: number,
+    received: number,
+    lastUpdate: string,
+    attribute: {
+        id: number,
+        name: string,
+    }
+}
+}
+
 export async function getServerSideProps(context: any) {
     const { steamid } = context.query
     // Fetch data from external API
@@ -59,16 +75,21 @@ export async function getServerSideProps(context: any) {
     const profileData: ProfileProps = await profileRes.json()
     const statsRes = await fetch(`https://api.intruderfps.com/agents/${steamid}/stats`)
     const statsData: StatsProps = await statsRes.json()
+    const voteRes = await fetch(`https://api.intruderfps.com/agents/${steamid}/votes`)
+    const voteData: VoteProps = await voteRes.json()
     
     // Pass data to the page via props
-    return { props: { profileData, statsData } }
+    return { props: { profileData, statsData, voteData } }
   }
 
-export default function Profile( {profileData, statsData}: { profileData: ProfileProps, statsData: StatsProps} ) {
+export default function Profile( {profileData, statsData, voteData}: { profileData: ProfileProps, statsData: StatsProps, voteData: VoteProps} ) {
+    const matchWinRate = Math.round((statsData.matchesWon / (statsData.matchesWon + statsData.matchesLost)) * 100)
+    const roundsWon = statsData.roundsWonCapture + statsData.roundsWonCustom + statsData.roundsWonElimination + statsData.roundsWonHack + statsData.roundsWonTimer
+    const roundWinRate = Math.round((roundsWon / (roundsWon + statsData.roundsLost + statsData.roundsTied)) * 100)
     return (
     <>
       <Head>
-        <title>Intruder Rooms | Profile</title>
+        <title>Intruder Rooms | {profileData.name}</title>
         <meta name="description" content="Live room data for Intruder." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -76,65 +97,110 @@ export default function Profile( {profileData, statsData}: { profileData: Profil
       <main className='min-h-screen bg-base-200'>
         <Nav />
         <div className='px-6'>
-        <div className='flex flex-row items-center py-5 gap-4'>
-            <div>
-                {profileData.status.online ? <div><div className="avatar online"><div className="w-24 rounded-full"><img src={profileData.avatarUrl}/></div></div></div> : <div className="avatar offline"><div className="w-24 rounded-full"><img src={profileData.avatarUrl}/></div></div>}
-            </div>
-            <div className="flex flex-col">
-                <div className='flex flex-row items-center gap-2'>
-                    <h1 className='font-bold text-lg'>{profileData.name}</h1>
-                    {profileData.role === 'Demoted' ? <div className="badge badge-secondary">Demoted</div> : profileData.role === 'AUG' ? <div className="badge badge-primary">AUG</div> : <div className="badge">Agent</div>}
+            <div className='flex flex-row items-center py-5 gap-4'>
+                <div>
+                    {profileData.status.online ? <div><div className="avatar online"><div className="w-24 rounded-full"><img src={profileData.avatarUrl}/></div></div></div> : <div className="avatar offline"><div className="w-24 rounded-full"><img src={profileData.avatarUrl}/></div></div>}
                 </div>
-                <div className='flex flex-row items-center gap-2'>
-                    <h2 className='font-medium text-gray-400'>Level {statsData.level}</h2>
-                    <div className="tooltip tooltip-bottom" data-tip={`${statsData.levelXp} XP / ${statsData.levelXpRequired} XP`}>
-                        <progress className="progress progress-success w-20" value={statsData.levelXp} max={statsData.levelXpRequired}></progress>
+                <div className="flex flex-col">
+                    <div className='flex flex-row items-center gap-2'>
+                        <h1 className='font-bold text-lg'>{profileData.name}</h1>
+                        {profileData.role === 'Demoted' ? <div className="badge badge-secondary">Demoted</div> : profileData.role === 'AUG' ? <div className="badge badge-primary">AUG</div> : <div className="badge">Agent</div>}
+                    </div>
+                    <div className='flex flex-row items-center gap-2'>
+                        <h2 className='font-medium text-gray-400'>Level {statsData.level}</h2>
+                        <div className="tooltip tooltip-bottom" data-tip={`${statsData.levelXp} XP / ${statsData.levelXpRequired} XP`}>
+                            <progress className="progress progress-success w-20" value={statsData.levelXp} max={statsData.levelXpRequired}></progress>
+                        </div>
+                    </div>
+                    <div className='flex flex-row items-center gap-2'>
+                        <div className=' text-green-400 text-sm font-medium'>+{voteData[0].positive}</div>
+                        <div className=' text-red-400 text-sm font-medium'>-{voteData[0].negative}</div>
+                        <div className=' text-gray-600 text-sm font-medium'>| {voteData[0].received} Votes</div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="stats shadow min-w-full"> 
-            <div className="stat">
-                <div className="stat-figure text-secondary">
+            <div className="stats stats-horizontal shadow max-w-full"> 
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    </div>
+                    <div className="stat-title">Kills</div>
+                    <div className="stat-value">{statsData.kills}</div>
                 </div>
-                <div className="stat-title">Kills</div>
-                <div className="stat-value">{statsData.kills}</div>
-            </div>
-            <div className="stat">
-                <div className="stat-figure text-secondary">
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    </div>
+                    <div className="stat-title">Deaths</div>
+                    <div className="stat-value">{statsData.deaths}</div>
                 </div>
-                <div className="stat-title">Deaths</div>
-                <div className="stat-value">{statsData.deaths}</div>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    </div>
+                    <div className="stat-title">K/D</div>
+                    <div className="stat-value">{(statsData.kills / statsData.deaths).toFixed(2)}</div>
+                </div>
             </div>
-        </div>
 
-        <div className="stats shadow min-w-full mt-3"> 
-            <div className="stat">
-                <div className="stat-figure text-secondary">
+            <div className='flex flex-row justify-between'>
+                <div className="stats stats-vertical shadow mt-3"> 
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title">Matches Won</div>
+                        <div className="stat-value">{statsData.matchesWon}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title">Matches Lost</div>
+                        <div className="stat-value">{statsData.matchesLost}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title mb-3">Match Win Rate</div>
+                        <div className='flex justify-center items-center'>
+                        <div className="radial-progress text-green-400 bg-black bg-opacity-10" style={{ "--value": matchWinRate, "--size": "7em"} as React.CSSProperties}>{matchWinRate}%</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="stat-title">Matches Won</div>
-                <div className="stat-value">{statsData.matchesWon}</div>
-            </div>
-            <div className="stat">
-                <div className="stat-figure text-secondary">
+                <div className="stats stats-vertical shadow mt-3"> 
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title">Rounds Won</div>
+                        <div className="stat-value">{roundsWon}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title">Rounds Lost</div>
+                        <div className="stat-value">{statsData.roundsLost}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                        </div>
+                        <div className="stat-title mb-3">Round Win Rate</div>
+                        <div className='flex justify-center items-center'>
+                        <div className="radial-progress text-blue-400 bg-black bg-opacity-10 border-black" style={{ "--value": roundWinRate, "--size": "7em"} as React.CSSProperties}>{roundWinRate}%</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="stat-title">Matches Lost</div>
-                <div className="stat-value">{statsData.matchesLost}</div>
             </div>
-        </div>
-        {/* <div className="stats shadow min-w-full">
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                </div>
-                <div className="stat-title">K/D</div>
-                <div className="stat-value">{(statsData.kills / statsData.deaths).toFixed(2)}</div>
-            </div>
-        </div> */}
 
-            {/* <div className="radial-progress" style={{"--value":80}}>80%</div> */}
-            {/* <p>Win Rate</p> */}
-        </div>
+            <div className="py-3 overflow-x-auto">
+                <table className="table table-zebra w-full table-fixed">
+                    <tbody>
+                        {Object.entries(statsData).map(([key, value]) => (
+                            <tr key={key}>
+                                <td>{key.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })}</td>
+                                <td>{value}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                </div>
+            </div>
       </main>
     </>
   )
