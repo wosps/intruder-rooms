ea3cd4bbd9a15a3c6d8091fa1a6a93290564b05b
Jordan Blewer <7325535+wosps@users.noreply.github.com>
2023-03-25 22:59:40 +0000

    Responsive Update (#4)

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,11 +4,13 @@ import { FaSteamSymbol } from 'react-icons/fa'
 export default function Nav() {
     return (
         <div className="navbar bg-base-100">
+        <div className='md:max-w-3xl md:mx-auto flex-1'>
         <div className="flex-1">
             <Link href="/" className="btn btn-ghost normal-case text-xl">Intruder Rooms</Link>
         </div>
         <div className="flex-none">
             <button className="btn btn-sm btn-outline" disabled><FaSteamSymbol className="mr-2"/>Sign In</button>
+        </div>
         </div>
         </div>
     )
