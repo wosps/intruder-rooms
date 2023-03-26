e84354a8850175c587642fb69a1e2a8807cf355f
Jord <7325535+wosps@users.noreply.github.com>
2023-03-26 15:47:35 +0100

    Temporarily removed Nav from 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,7 +15,6 @@ export default function FourOhFour() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='min-h-screen bg-base-200'>
-        <Nav />
         <div className='px-6 h-[calc(100vh-100px)] flex flex-col items-center justify-center'>
             <div>
                 <h1 className='text-4xl font-medium text-gray-200'>Error 404!</h1>
